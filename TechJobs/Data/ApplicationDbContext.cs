ac4410af17e1b1d970d68042257f127e1f39c81d
andrew <andrewjharris96@gmail.com>
2021-11-23 22:12:04 -0500

    time to rebuild

---
+++ TechJobs/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TechJobs.Models;
+
+namespace TechJobs.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<Job> Jobs { get; set; }
+        public DbSet<Employer> Employers { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<JobSkill> JobSkills { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<JobSkill>()
+                .HasKey(j => new { j.JobId, j.SkillId });
+            
+        }
+    }
+}
---
+++ TechJobs/Models/JobSkill.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace TechJobs.Models
+{
+    public class JobSkill
+    {
+        public int JobId { get; set; }
+        public Job Job { get; set; }
+
+        public int SkillId { get; set; }
+        public Skill Skill { get; set; }
+
+        public JobSkill()
+        {
+        }
+
+        public JobSkill(int jobId, int skillId)
+        {
+            JobId = jobId;
+            SkillId = skillId;
+        }
+    }
+}
